1688726c258b613625fffa855d8dc694f529e0e3
ewycheung <cheung_eddie@rogers.com>
2021-04-04 22:37:03 -0400

    Add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update ]
+
+  def show    
+  end
+
   def new
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
+  def edit    
   end
 
-  def update
-    @user = User.find(params[:id])
+  def update    
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to articles_path
@@ -28,6 +31,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:username, :email, :password)
